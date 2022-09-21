10c2a49a302f4eb8fa236f908f51414f35b6145c
kh <moltak@gmail.com>
2022-09-21 17:12:57 +0900

    초기 커밋.

---
+++ WinFormsApp1/ScreenSaverForm.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace WinFormsApp1
+{
+    partial class ScreenSaverForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textLabel = new System.Windows.Forms.TextBox();
+            this.moveTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // textLabel
+            // 
+            this.textLabel.BackColor = System.Drawing.Color.Black;
+            this.textLabel.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textLabel.Font = new System.Drawing.Font("맑은 고딕", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textLabel.ForeColor = System.Drawing.Color.IndianRed;
+            this.textLabel.Location = new System.Drawing.Point(312, 198);
+            this.textLabel.Name = "textLabel";
+            this.textLabel.Size = new System.Drawing.Size(159, 54);
+            this.textLabel.TabIndex = 0;
+            this.textLabel.Text = "Demo";
+            this.textLabel.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // ScreenSaverForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "ScreenSaverForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.ScreenSaverForm_Load);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.ScreenSaverForm_KeyPress);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.ScreenSaverForm_MouseClick);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.ScreenSaverForm_MouseMove);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox textLabel;
+        private System.Windows.Forms.Timer moveTimer;
+    }
+}
---
+++ WinFormsApp1/ScreenSaverForm.cs
@@ -0,0 +1,95 @@
+using System.Runtime.InteropServices;
+
+namespace WinFormsApp1
+{
+    public partial class ScreenSaverForm : Form
+    {
+        private Point mouseLocation;
+        private Random rand = new Random();
+        private bool previewMode = false;
+
+        [DllImport("user32.dll")]
+        static extern IntPtr SetParent(IntPtr hWndChild, IntPtr hWndNewParent);
+
+        [DllImport("user32.dll")]
+        static extern int SetWindowLong(IntPtr hWnd, int nIndex, IntPtr dwNewLong);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+
+        [DllImport("user32.dll")]
+        static extern bool GetClientRect(IntPtr hWnd, out Rectangle lpRect);
+
+
+        public ScreenSaverForm(Rectangle Bounds)
+        {
+            InitializeComponent();
+            this.Bounds = Bounds;
+        }
+
+        public ScreenSaverForm(IntPtr PreviewWndHandle)
+        {
+            InitializeComponent();
+
+            // Set the preview window as the parent of this window
+            SetParent(this.Handle, PreviewWndHandle);
+
+            // Make this a child window so it will close when the parent dialog closes
+            // GWL_STYLE = -16, WS_CHILD = 0x40000000
+            SetWindowLong(this.Handle, -16, new IntPtr(GetWindowLong(this.Handle, -16) | 0x40000000));
+
+            // Place our window inside the parent
+            Rectangle ParentRect;
+            GetClientRect(PreviewWndHandle, out ParentRect);
+            Size = ParentRect.Size;
+            Location = new Point(0, 0);
+
+            // Make text smaller
+            textLabel.Font = new System.Drawing.Font("Arial", 6);
+
+            previewMode = true;
+        }
+
+        private void ScreenSaverForm_Load(object sender, EventArgs e)
+        {
+            Cursor.Hide();
+            TopMost = true;
+
+            moveTimer.Interval = 3000;
+            moveTimer.Tick += new EventHandler(moveTimer_Tick);
+            moveTimer.Start();
+        }
+
+        private void ScreenSaverForm_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (!mouseLocation.IsEmpty)
+            {
+                // Terminate if mouse is moved a significant distance
+                if (Math.Abs(mouseLocation.X - e.X) > 5 ||
+                    Math.Abs(mouseLocation.Y - e.Y) > 5)
+                    Application.Exit();
+            }
+
+            // Update current mouse location
+            mouseLocation = e.Location;
+        }
+
+        private void ScreenSaverForm_MouseClick(object sender, MouseEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void ScreenSaverForm_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void moveTimer_Tick(object sender, System.EventArgs e)
+        {
+            // Move text to new location
+            textLabel.Left = rand.Next(Math.Max(1, Bounds.Width - textLabel.Width));
+            textLabel.Top = rand.Next(Math.Max(1, Bounds.Height - textLabel.Height));
+        }
+
+    }
+}
